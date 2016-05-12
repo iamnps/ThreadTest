7df3ccd4c2e67f9c13810692828e7e7fe10bd9ef
iamnps <niepeishen@sina.com>
2016-05-12 16:01:16 +0800

    thread code

---
+++ src/First/LiftOff.java
@@ -0,0 +1,29 @@
+package First;
+
+public class LiftOff implements Runnable {
+	
+	protected int countDown = 10;
+	private static int tachCount = 0;
+	private final int id = tachCount++;
+	
+	public LiftOff(){
+	
+	}
+	
+	public LiftOff(int countDown){
+		this.countDown = countDown;
+	}
+
+	public String status(){
+		return "#" + id + "(" + (countDown > 0 ? countDown : "LiftOff!") + ").";
+	}
+	
+	@Override
+	public void run() {
+		while(countDown-- > 0){
+			System.out.println(status());
+			Thread.yield();
+		}
+	}
+
+}
