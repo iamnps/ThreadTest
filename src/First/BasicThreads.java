3e21a9a72cc4e4ccaaa4a684ddd9c44760ae77d5
iamnps <niepeishen@sina.com>
2016-05-12 17:08:06 +0800

    thread test

---
+++ src/First/BasicThreads.java
@@ -0,0 +1,11 @@
+package First;
+
+public class BasicThreads {
+	
+	public static void main(String[] args) {
+		Thread t = new Thread(new LiftOff());
+		t.start();
+		System.out.println("Waiting for LiftOff");
+	}
+
+}
--- src/First/LiftOff.java
+++ src/First/LiftOff.java
@@ -21,7 +21,7 @@ public class LiftOff implements Runnable {
 	@Override
 	public void run() {
 		while(countDown-- > 0){
-			System.out.println(status());
+			System.out.print(status());
 			Thread.yield();
 		}
 	}
