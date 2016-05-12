7923d2aaf71d8f492bbe0034f371b18859520a5f
iamnps <niepeishen@sina.com>
2016-05-12 18:00:57 +0800

    thread test

--- src/First/BasicThreads.java
+++ src/First/BasicThreads.java
@@ -2,7 +2,7 @@ package First;
 
 public class BasicThreads {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		Thread t = new Thread(new LiftOff());
 		t.start();
 		System.out.println("Waiting for LiftOff");
