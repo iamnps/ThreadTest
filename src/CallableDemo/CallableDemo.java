fa383e3e7eee3e20e88c2db24d6dd7e74a4c1f41
iamnps <niepeishen@sina.com>
2016-05-16 09:53:51 +0800

    callable demo

---
+++ src/CallableDemo/CallableDemo.java
@@ -0,0 +1,21 @@
+package CallableDemo;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class CallableDemo {
+
+	public static void main(String[] args) {
+		ExecutorService exec = Executors.newCachedThreadPool();
+		List<Future<String>> results = new ArrayList<Future<String>>();
+		for (int i = 0; i < 10; i++) {
+			results.add(exec.submit(new TaskWithResult(i)));
+		}
+		for(Future<String> fs : results){
+			
+		}
+	}
+}
